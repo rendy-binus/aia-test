74425f84d4f29eea2e4f962014169bf005c1d369
Rendy Eza Putra <rendy.putra@binus.ac.id>
2022-07-30 18:10:56 +0700

    Initial commit

---
+++ src/main/java/com/example/aiatest/AiaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aiatest/AiaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
